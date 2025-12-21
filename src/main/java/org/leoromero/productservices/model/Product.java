a90f7a773a627751ab4c6c25a412925900730438
Leonardo Vargas Olvera <leonardo.vargas.uls@gmail.com>
2025-12-20 19:05:05 -0600

    Creacion del model y repositoy product

---
+++ src/main/java/org/leoromero/productservices/model/Product.java
@@ -0,0 +1,26 @@
+package org.leoromero.productservices.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Table(name = "products")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String name;
+    private BigDecimal price;
+    private Long stock;
+
+
+
+}
+
