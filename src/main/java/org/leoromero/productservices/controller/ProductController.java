bc01fb6f5ca036ced95d22a07393048edc2626bb
Leonardo Vargas Olvera <leonardo.vargas.uls@gmail.com>
2025-12-28 12:33:30 -0600

    Configutacion de la clase securityconfig

--- src/main/java/org/leoromero/productservices/controller/ProductController.java
+++ src/main/java/org/leoromero/productservices/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/product")
+@RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
     private static final Logger log = LoggerFactory.getLogger(ProductController.class);
